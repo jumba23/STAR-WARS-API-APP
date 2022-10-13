27ff152ac4f41fe6946c70440aabf5eef2d12e26
Goran SD <g.cvetic23@gamail.com>
2022-10-13 12:51:03 -0700

    before Link router dom

--- src/components/DisplayData/DisplayData.js
+++ src/components/DisplayData/DisplayData.js
@@ -1,5 +1,4 @@
 import "./displaydata.css";
-import Card from "react-bootstrap/Card";
 
 const DisplayData = (props) => {
   const categoryDataArray = [...props.categoryData];
@@ -12,29 +11,12 @@ const DisplayData = (props) => {
             <li key={element}>{element}</li>
           ))}
         </ul>
-        <Card style={{ width: "18rem" }} bg="dark" className="mb-2" >
-          <Card.Body >
-            <Card.Title className= "fs-6 fw-bolder fst-italic">More Info</Card.Title>
-            <Card.Text className="text-start fs-6 fw-normal lh-sm">
-              Name: Mike Jordan
-            </Card.Text>
-            <Card.Text className="text-start fs-6 fw-normal lh-sm">
-              Birth Date: 04-93-1960
-             </Card.Text>
-            <Card.Text className="text-start fs-6 fw-normal lh-sm">
-              Mass: 192
-             </Card.Text>
-            <Card.Text className="text-start fs-6 fw-normal lh-sm">
-              Height: 34
-              </Card.Text>
-            <Card.Text className="text-start fs-6 fw-normal lh-sm">
-              Homeworld: Planet Earth 
-             </Card.Text>
-            <Card.Text className="text-start fs-6 fw-normal lh-lg text-break">
-              Species:"Human"
-             </Card.Text>
-          </Card.Body>
-        </Card>
+        <div className="search-data-details">
+          <h5>Selection details</h5>
+          <p>Name: </p>
+          <p>Birth Year: </p>
+          <p>Mass: </p>
+        </div>
       </div>
     </>
   );
