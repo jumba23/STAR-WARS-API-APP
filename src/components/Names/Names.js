e1e3636968eaf1c98e6bea2fa68f463ae4e68344
Goran SD <g.cvetic23@gamail.com>
2022-10-14 10:08:43 -0700

    Names element added

--- src/components/Names/Names.js
+++ src/components/Names/Names.js
@@ -1,6 +1,17 @@
+import { NavLink } from "react-router-dom";
+
 const Names = ({list}) => {
     return ( <>
-    <div>names page</div>
+    <div className="results">
+      {list.length === 0 && <p>No people...</p>}
+      {list.map((item, index) => (
+        <p key={item.name}>
+          <NavLink className="activeLink" to={`/people/${index + 1}`}>
+            {item}
+          </NavLink>
+        </p>
+      ))}
+    </div>
     </> );
 }
  
